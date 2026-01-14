4ae0996fdf2486d5d278f7fd63c067a19ed630c2
grinyya27-gif <grinyya27@gmail.com>
2026-01-14 18:44:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+const game = {
+    gold: 0,
+    emeralds: 0,
+
+    updateUI() {
+        document.getElementById('gold').innerText = Math.floor(this.gold);
+        document.getElementById('emeralds').innerText = this.emeralds;
+    },
+
+    // Универсальная функция для работы
+    work(type) {
+        if (type === 'mine') {
+            // ЛОГИКА РУДНИКА
+            this.gold += 10;
+            // Шанс 15% найти изумруд
+            if (Math.random() < 0.15) {
+                this.emeralds += 1;
+                tg.HapticFeedback.notificationOccurred('success'); // Сильная вибрация
+            } else {
+                tg.HapticFeedback.impactOccurred('medium'); // Средняя вибрация
+            }
+        } 
+        else if (type === 'fish') {
+            // ЛОГИКА РЫБАЛКИ
+            this.gold += 15; // В порту золота чуть больше
+            tg.HapticFeedback.impactOccurred('light'); // Легкая вибрация
+        }
+
+        this.updateUI();
+    }
+};
+
+// Инициализация при старте
+game.updateUI();
 
